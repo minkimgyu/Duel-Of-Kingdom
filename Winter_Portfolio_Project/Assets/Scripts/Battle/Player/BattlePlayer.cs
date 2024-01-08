0263818edd2d1013d89cc761bfdf687f58adac4f
GeonhanLee <leegeonhan1122@gmail.com>
2024-01-09 07:18:31 +0900

    add BattlePlayer class

---
+++ Winter_Portfolio_Project/Assets/Scripts/Battle/Player/BattlePlayer.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace WPP.Battle
+{
+    public class BattlePlayer : MonoBehaviour
+    {
+        [Header("Tower System")]
+        [SerializeField] private TowerSystem _towerSystem;
+    }
+}
+

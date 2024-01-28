e47f1ccbdf7a3c28ca7f608b645fd1c8b778b525
zino <luboy@naver.com>
2024-01-28 12:59:41 +0900

    add gridSize and unit information

--- Winter_Portfolio_Project/ClashRoyale_Server/ClashRoyale_Server/Data/ClientInfo/CardData/Card.cs
+++ Winter_Portfolio_Project/ClashRoyale_Server/ClashRoyale_Server/Data/ClientInfo/CardData/Card.cs
@@ -1,30 +1,52 @@
 ﻿
-using ClashRoyale_Server.Database.Units;
+using WPP.ClashRoyale_Server.Data.Units;
+using System;
+using System.Text.Json.Serialization;
 
 namespace WPP.ClashRoyale_Server.Data.ClientInfo.CardData
 {
     public enum CardType
     {
-        Troop, Spell, Building
+        troop, attack_building, spawn_building, spell
     }
     public enum CardRarity
     {
-        Common, Rare, Epic, Legendary
+        common, rare, epic, legendary
     }
-    public struct Vector2Int
-    {
-        public int x {get; set;}
-        public int y {get; set;}
 
-        public Vector2Int One()
+    public struct GridSize
+    {
+        public int top { get; set; }
+        public int down { get; set; }
+        public int left { get; set; }
+        public int right { get; set; }
+
+        public GridSize TroopGrid()
         {
-            return new Vector2Int { x = 1, y = 1 };
+            GridSize gridSize = new GridSize();
+            gridSize.top = 0;
+            gridSize.down = 0;
+            gridSize.left = 0;
+            gridSize.right = 0;
+            return gridSize;
+        }
+
+        public GridSize BuildingGrid()
+        {
+            GridSize gridSize = new GridSize();
+            gridSize.top = 1;
+            gridSize.down = -1;
+            gridSize.left = -1;
+            gridSize.right = 1;
+            return gridSize;
         }
     }
-
     class Card
     {
         public int id { get; set; }
+        public int unit_id { get; set; }
+
+        public Unit unit { get; set; }
 
         public CardType type { get; set; }
 
@@ -32,12 +54,15 @@ namespace WPP.ClashRoyale_Server.Data.ClientInfo.CardData
 
         public int needElixir { get; set; }
 
-        public Vector2Int gridSize { get; set; }
+        public GridSize gridSize { get; set; }
 
         public Card() { }
-        public Card(int id, CardType type, CardRarity rarity, int needElixir, Vector2Int gridSize)
+        public Card(int id, int unit_id, CardType type, CardRarity rarity, int needElixir, GridSize gridSize)
         {
             this.id = id;
+            this.unit_id = unit_id;
+            this.type = type;
+            unit = DatabaseManager.Instance().FindUnit(type, unit_id);
             this.rarity = rarity;
             this.needElixir = needElixir;
             this.gridSize = gridSize;

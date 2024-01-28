a288dec4ec371774bc099664833f26cb835e5495
zino <luboy@naver.com>
2024-01-28 12:21:48 +0900

    add client_test scenes

--- Winter_Portfolio_Project/Assets/Scripts/AI/TilePiece.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TilePiece : MonoBehaviour
-{
-    [SerializeField] bool isBlock;
-    public bool IsBlock { get { return isBlock; } }
-}

12c43e0af2b5000f48367be6a4fda22806c10a60
zino <luboy@naver.com>
2024-02-09 02:16:49 +0900

    update card datas

--- Winter_Portfolio_Project/Assets/Scripts/TempClient/ClientInfo/Card/CardData.cs
+++ Winter_Portfolio_Project/Assets/Scripts/TempClient/ClientInfo/Card/CardData.cs
@@ -8,7 +8,7 @@ namespace WPP.ClientInfo.Card
 {
     public enum CardType
     {
-        troop, multiple_troops, attack_building, spawn_building, spell
+        troop, attack_building, spawn_building, spell
     }
     public enum CardRarity
     {

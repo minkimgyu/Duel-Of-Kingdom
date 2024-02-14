96d44f87609c79cae5f216206aa7d8476817a79d
GeonhanLee <leegeonhan1122@gmail.com>
2024-02-14 20:30:46 +0900

    add game over ui showing crown count

---
+++ Winter_Portfolio_Project/Assets/Scripts/UI/GameOverUI.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WPP.Battle.UI
+{
+    public class GameOverUI : MonoBehaviour
+    {
+        [SerializeField] private GameObject _gameOverPanel;
+        [Space]
+        [SerializeField] private GameObject _playerCrown1;
+        [SerializeField] private GameObject _playerCrown2;
+        [SerializeField] private GameObject _playerCrown3;
+        [Space]
+        [SerializeField] private GameObject _opponentCrown1;
+        [SerializeField] private GameObject _opponentCrown2;
+        [SerializeField] private GameObject _opponentCrown3;
+
+        private BattleManager _battleManager;
+        private void OnEnable()
+        {
+            _battleManager = BattleManager.Instance();
+            _battleManager.OnGameOver += OnGameOver;
+        }
+
+        private void OnDisable()
+        {
+            _battleManager.OnGameOver -= OnGameOver;
+        }
+
+        private void OnGameOver(BattleResult result)
+        {
+            _gameOverPanel.SetActive(true);
+
+            int playerCrown = _battleManager.Player.CrownSystem.CrownCount;
+            int opponentCrown = _battleManager.Opponent.CrownSystem.CrownCount;
+            
+            _playerCrown1.SetActive(playerCrown >= 1);
+            _playerCrown2.SetActive(playerCrown >= 2);
+            _playerCrown3.SetActive(playerCrown >= 3);
+
+            _opponentCrown1.SetActive(opponentCrown >= 1);
+            _opponentCrown2.SetActive(opponentCrown >= 2);
+            _opponentCrown3.SetActive(opponentCrown >= 3);
+        }
+    }
+}

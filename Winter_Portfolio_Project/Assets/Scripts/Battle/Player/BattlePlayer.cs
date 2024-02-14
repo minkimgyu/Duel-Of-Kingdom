1e28f6f5e3ae65c6ca58d57128969e310225f1f8
GeonhanLee <leegeonhan1122@gmail.com>
2024-02-14 21:34:05 +0900

    send message when opponent tower gets destroyed

--- Winter_Portfolio_Project/Assets/Scripts/Battle/Player/BattlePlayer.cs
+++ Winter_Portfolio_Project/Assets/Scripts/Battle/Player/BattlePlayer.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using WPP.Network;
 
 namespace WPP.Battle
 {
@@ -9,11 +10,33 @@ namespace WPP.Battle
         [SerializeField] private CrownSystem _crownSystem;
         [SerializeField] private TowerSystem _towerSystem;
 
-        public void Init()
+        public void Init(bool isClient)
         {
             _enemy._towerSystem.OnKingTowerDestroyed += _crownSystem.AddCrown;
             _enemy._towerSystem.OnLeftPrincessTowerDestroyed += _crownSystem.AddCrown;
             _enemy._towerSystem.OnRightPrincessTowerDestroyed += _crownSystem.AddCrown;
+
+            if (!isClient)
+            {
+                _enemy._towerSystem.OnKingTowerDestroyed += OnOpponentKingTowerDestroyed;
+                _enemy._towerSystem.OnLeftPrincessTowerDestroyed += OnOpponentLeftTowerDestroyed;
+                _enemy._towerSystem.OnRightPrincessTowerDestroyed += OnOpponentRightTowerDestroyed;
+            }
+        }
+
+        private void OnOpponentRightTowerDestroyed()
+        {
+            ClientTCP.Instance().SendDataToServer(Client_PacketTagPackages.C_DESTROY_OPPONENT_RIGHT_TOWER);
+        }
+
+        private void OnOpponentLeftTowerDestroyed()
+        {
+            ClientTCP.Instance().SendDataToServer(Client_PacketTagPackages.C_DESTROY_OPPONENT_LEFT_TOWER);
+        }
+
+        private void OnOpponentKingTowerDestroyed()
+        {
+            ClientTCP.Instance().SendDataToServer(Client_PacketTagPackages.C_DESTROY_OPPONENT_KING_TOWER);
         }
 
         public CrownSystem CrownSystem => _crownSystem;
